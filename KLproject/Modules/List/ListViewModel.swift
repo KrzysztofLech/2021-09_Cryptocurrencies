46d55192263c671ba8bae1a4c3e372b729fed7fa
Krzysztof Lech <krzysztof.lech@24abc.pl>
2021-09-02 08:12:46 +0200

    Init commit

---
+++ KLproject/Modules/List/ListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ListViewModel.swift
+//  KLproject
+//
+//  Created by KL on 02/09/2021.
+//
+
+import Foundation
+
+protocol ListViewModelProtocol {
+}
+
+final class ListViewModel: ListViewModelProtocol {
+}
